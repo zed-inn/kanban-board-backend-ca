5124b24c8a7b1628b94a8ba41ac7a4918681a702
Zed <skhull.coding@gmail.com>
2026-03-02 22:28:01 +0530

    feat: link app router

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import app from "@config/app";
 import { env } from "@config/env";
 import { initRepos } from "@shared/initializer/repo.initializer";
 import { setupIo } from "./io-server";
+import { AppRouter } from "@shared/router/app.router";
 
 const startServer = async () => {
   try {
@@ -11,6 +12,7 @@ const startServer = async () => {
     await db.connect();
     await initRepos();
 
+    app.register(AppRouter);
     await app.listen({ port: env.PORT, host: env.HOST });
   } catch (error) {
     app.log.error(error);
