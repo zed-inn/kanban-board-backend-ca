9ca7fbb72649294788d85ffa62023798c0a0ece0
Zed <skhull.coding@gmail.com>
2026-03-02 20:14:01 +0530

    feat: postgres policies interface implementation

---
+++ src/interfaces/policy/column.policy.ts
@@ -0,0 +1,30 @@
+import db from "@config/db";
+import { ColumnPolicy } from "kanban";
+import {
+  ColumnNotInBoardError,
+  InvalidColumnPositionError,
+} from "kanban/src/core/errors/column.error";
+
+export class PostgresColumnPolicy implements ColumnPolicy {
+  ensureColumnInBoard = async (
+    columnId: string,
+    boardId: string,
+  ): Promise<void> => {
+    const res = await db.query(
+      "SELECT * FROM columns WHERE id = $1 AND board_id = $2;",
+      [columnId, boardId],
+    );
+    if (res.rowCount === 0) throw new ColumnNotInBoardError();
+  };
+
+  ensureEmptyPositionInBoard = async (
+    position: number,
+    boardId: string,
+  ): Promise<void> => {
+    const res = await db.query(
+      "SELECT * FROM columns WHERE position = $1 AND board_id = $2;",
+      [position, boardId],
+    );
+    if (res.rowCount !== 0) throw new InvalidColumnPositionError();
+  };
+}
