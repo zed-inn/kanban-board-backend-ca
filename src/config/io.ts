2c043784f7b8da942913ae1ab9bee507bdfde0bb
Zed <skhull.coding@gmail.com>
2026-03-03 14:31:39 +0530

    feat: io auth middleware

--- src/config/io.ts
+++ src/config/io.ts
@@ -1,7 +1,12 @@
 import app from "./app";
 import { IoServer } from "@shared/initializer/io.initializer";
+import { authenticate } from "@shared/io-middleware/authenticate.middleware";
 import { SocketData } from "@shared/types/socket";
 
-export const io = IoServer<SocketData>(app.server, {
+const ioServer = IoServer<SocketData>(app.server, {
   cors: { credentials: true },
 });
+
+ioServer.use(authenticate);
+
+export const io = ioServer;
