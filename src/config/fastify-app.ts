6f00d7d4b37c4a86b5b35ea542a4a3e477c1df63
Zed <skhull.coding@gmail.com>
2026-03-06 11:42:51 +0530

    fix: set the zod provider as provider in fastify app

--- src/config/fastify-app.ts
+++ src/config/fastify-app.ts
@@ -2,11 +2,20 @@ import fastify from "fastify";
 import fastifyCookie from "@fastify/cookie";
 import { env } from "./env";
 import { authenticate } from "@shared/middlewares/fastify-authenticate.plugin";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
 
 const app = fastify({
   logger: env.NODE_ENV === "dev",
   trustProxy: env.NODE_ENV == "prod", // Behind Servers
 });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.decorateRequest("user", undefined);
 
 app.register(fastifyCookie);
 app.register(authenticate);
