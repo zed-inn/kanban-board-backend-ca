c8334ebb28fea084a0ba64e308f69efb834592a9
Zed <skhull.coding@gmail.com>
2026-03-12 03:25:22 +0530

    fix: extends application error

--- src/routes/auth/auth.error.ts
+++ src/routes/auth/auth.error.ts
@@ -8,9 +8,9 @@ export class PasswordNotMatchedError extends ApplicationError {
   }
 }
 
-export class EmailAlreadyUsedError extends Error {
+export class EmailAlreadyUsedError extends ApplicationError {
   readonly error = "conflict";
-  message: string = "EMAIL_ALREADY_USED";
+  readonly code = "EMAIL_ALREADY_USED";
   constructor() {
     super("This email is already used in another account.");
   }
