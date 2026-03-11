5b24a729bb7f60d43a74be5e9f2c5eb152f60d99
Zed <skhull.coding@gmail.com>
2026-03-12 03:36:01 +0530

    fix: join board when member

--- src/io-server.setup.ts
+++ src/io-server.setup.ts
@@ -1,11 +1,23 @@
+import { memberRepo } from "@config/core";
 import app from "@config/fastify-app";
 import { io } from "@config/io-server";
+import { BoardMembership } from "kanban";
 
 export const setupIo = () => {
   io.on("connection", async (socket) => {
     app.log.info(`Socket connected: ${socket.id}`);
 
     if (socket.data.user?.id) socket.join(`user:${socket.data.user.id}`);
+
+    socket.on("join_board", async (boardId: string) => {
+      if (socket.data.user?.id) {
+        const isMember = await memberRepo.exists(
+          new BoardMembership({ boardId, memberId: socket.data.user.id }),
+        );
+
+        if (isMember) socket.join(`board:${boardId.trim()}`);
+      }
+    });
 
     socket.on("disconnect", async () => {
       app.log.info(`Socket disconnected: ${socket.id}`);
