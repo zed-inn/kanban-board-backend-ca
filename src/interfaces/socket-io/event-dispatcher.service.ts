129a2209cd42328a16f482dd8d6962929ad44acd
Zed <skhull.coding@gmail.com>
2026-03-12 03:39:31 +0530

    feat: events dispatched done

--- src/interfaces/socket-io/event-dispatcher.service.ts
+++ src/interfaces/socket-io/event-dispatcher.service.ts
@@ -2,9 +2,22 @@ import { BaseEvent, EventDispatcher } from "kanban";
 import { Server } from "socket.io";
 
 export class SocketIoEventDispatcher implements EventDispatcher {
-  constructor(private ioServer: Server) {}
+  constructor(private io: Server) {}
 
   async emit(event: BaseEvent<any>): Promise<void> {
     const eventJson = event.toJSON();
+
+    if (Array.isArray(eventJson.target))
+      this.io
+        .to(eventJson.target.map((t) => (t.type === "room" ? t.name : t.id.v)))
+        .emit(eventJson.name, eventJson.data);
+    else
+      this.io
+        .to(
+          eventJson.target.type === "room"
+            ? eventJson.target.name
+            : eventJson.target.id.v,
+        )
+        .emit(eventJson.name, eventJson.data);
   }
 }
